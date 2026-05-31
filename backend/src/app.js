02b5be09da987cf68b8cdb5feffb9361808b7a1b
Srinath <vulapusrinath@gmail.com>
2026-05-31 21:50:46 +0530

    Refactor error handling and clean up imports in app.js and implemented login functionality in react

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,3 +1,4 @@
+import "dotenv/config";
 import express from "express";
 import cookieParser from "cookie-parser";
 import cors from "cors";
@@ -12,6 +13,7 @@ app.use(cors({
 app.use(express.json({ limit: "32kb" }));
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+
 
 import userRouter from "./routes/user.route.js";
 app.use("/api/v1/user", userRouter);
@@ -30,8 +32,6 @@ app.use((req, res) => {
 });
 
 app.use((err, req, res, next) => {
-    console.error(err);
-
     res.status(err.statusCode || 500).json({
         success: false,
         message: err.message || "Internal Server Error",
